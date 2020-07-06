af88bd023fefe76445364e9b88e1a9c372c062dc
JeremyZelayaRodriguez <JeremyZelayaRodrigue@WindowsVS2019>
2020-07-05 23:58:47 -0600

    Created android project, set up manifest and a basic gmaps config.

---
+++ GoGo App/MainActivity.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Android.App;
+using Android.Gms.Maps;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace GoGo_App
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+        private GoogleMap mMap;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.gmap);
+            //var mapFragment = (MapFragment) FragmentManager.FindFragmentById(Resource.Id.gmap);
+            
+            mapFragment.GetMapAsync(this);
+
+            //Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            //SetSupportActionBar(toolbar);
+
+            //FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            //fab.Click += FabOnClick;
+        }
+
+        public void OnMapReady(GoogleMap map)
+        {
+            // Do something with the map, i.e. add markers, move to a specific location, etc.
+            // Set up a normal map
+            map.MapType = GoogleMap.MapTypeHybrid;
+            // Map settings
+            map.UiSettings.ZoomControlsEnabled = true;
+            map.UiSettings.CompassEnabled = true;
+        }
+
+        //public override bool OnCreateOptionsMenu(IMenu menu)
+        //{
+        //    MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+        //    return true;
+        //}
+
+        //public override bool OnOptionsItemSelected(IMenuItem item)
+        //{
+        //    int id = item.ItemId;
+        //    if (id == Resource.Id.action_settings)
+        //    {
+        //        return true;
+        //    }
+
+        //    return base.OnOptionsItemSelected(item);
+        //}
+
+        //private void FabOnClick(object sender, EventArgs eventArgs)
+        //{
+        //    View view = (View) sender;
+        //    Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+        //        .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        //}
+
+        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
+	}
+}
